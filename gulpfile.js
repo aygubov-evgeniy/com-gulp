f47218fd10273710a02a6f45892743670a7e2253
gitZhekos <aigubov.zheka@yandex.ru>
2019-05-13 02:44:33 +0300

    v2

--- gulpfile.js
+++ gulpfile.js
@@ -5,37 +5,120 @@ watch      = require('gulp-watch'),
 concat     = require('gulp-concat'),
 uglify     = require('gulp-uglify'),
 sourcemaps = require('gulp-sourcemaps'),
-prefixer = require('gulp-autoprefixer'),
+postcss    = require('gulp-postcss'),
+cleanCSS = require('gulp-clean-css'),
 mqpacker   = require('css-mqpacker'),
-cssmin = require('gulp-minify-css'),
-babel = require('gulp-babel'),
 sortCSSmq  = require('sort-css-media-queries'),
-autopolyfiller = require('gulp-autopolyfiller'),
-merge = require('event-stream').merge;
+babel = require('gulp-babel'),
+plumber = require('gulp-plumber'),
+sortCSSmq  = require('sort-css-media-queries'),
+streamqueue = require('streamqueue'),
+rimraf     = require('rimraf');
 
 var path = {
   dist: {
+    distDir: 'dist/',
     jsDir: 'dist/js/',
+    jsMain: 'main.js',
     cssDir: 'dist/css/',
+    cssMain: 'main.css',
   },
   src: {
-    js: 'src/js/**/*.js',
-    css: 'src/css/**/*.css',
+    jsExclude: ['assets/js/jquery.js'],
+    js: ['assets/js/**/*.js', '!assets/js/jquery.js'],
+    cssExclude: 'assets/css/single/*.css',
+    css: ['assets/css/**/*.css', '!assets/css/single/*.css'],
   },
   watch: {
-    js: 'src/js/**/*.js',
-    css: 'src/css/**/*.css',
+    js: 'assets/js/**/*.js',
+    css: 'assets/css/**/*.css',
   }
 };
 
-gulp.task('js:build', function () {
-  gulp.src(path.src.js)
-    .pipe(concat('main.js'))
-    .pipe(babel({
-      presets: ['@babel/preset-env']
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(path.dist.jsDir))
+var processors_dev = [
+  mqpacker({
+    sort: sortCSSmq.desktopFirst
+  }),
+]
+
+gulp.task('js:dev', function () {
+  return streamqueue({ objectMode: true },
+    gulp.src(path.src.js)
+      .pipe(plumber())
+      .pipe(sourcemaps.init())
+      .pipe(concat(path.dist.jsMain))
+      .pipe(babel({
+        presets: ['@babel/preset-env']
+      }))
+      .pipe(sourcemaps.write('.')),
+
+    gulp.src(path.src.jsExclude)
+  )
+    .pipe(gulp.dest(path.dist.jsDir));
 });
+
+gulp.task('css:dev', function () {
+  return streamqueue({ objectMode: true },
+    gulp.src(path.src.css)
+      .pipe(plumber())
+      .pipe(sourcemaps.init())
+      .pipe(concat(path.dist.cssMain))
+      .pipe(postcss(processors_dev))
+      .pipe(sourcemaps.write('.')),
+
+    gulp.src(path.src.cssExclude)
+  )
+    .pipe(gulp.dest(path.dist.cssDir));
+});
+
+gulp.task('js:prod', function () {
+  return streamqueue({ objectMode: true },
+    gulp.src(path.src.js)
+      .pipe(plumber())
+      .pipe(concat(path.dist.jsMain))
+      .pipe(babel({
+        presets: ['@babel/preset-env']
+      }))
+      .pipe(uglify()),
+
+    gulp.src(path.src.jsExclude)
+      .pipe(uglify())
+  )
+    .pipe(gulp.dest(path.dist.jsDir));
+});
+
+gulp.task('css:prod', function () {
+  return streamqueue({ objectMode: true },
+    gulp.src(path.src.css)
+      .pipe(plumber())
+      .pipe(concat(path.dist.cssMain))
+      .pipe(postcss(processors_dev))
+      .pipe(cleanCSS()),
+
+    gulp.src(path.src.cssExclude)
+      .pipe(cleanCSS())
+  )
+    .pipe(gulp.dest(path.dist.cssDir));
+});
+
+gulp.task('dev', [
+          'js:dev',
+          'css:dev'
+]);
+
+gulp.task('build', function(cd){
+  rimraf(path.dist.distDir, cd);
+  gulp.start('js:prod');
+  gulp.start('css:prod');
+});
+
+gulp.task('watch', function(){
+  watch([path.watch.js], function(event, cb) {
+    gulp.start('js:dev');
+  });
+  watch(path.watch.css, function(event, cb) {
+    gulp.start('css:dev');
+  });
+});
+
+gulp.task('default', ['dev','watch']);
